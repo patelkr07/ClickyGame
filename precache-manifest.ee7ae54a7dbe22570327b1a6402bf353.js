64a643e46be94b465741261216ba795d899031aa
Kunal <patelkr07@gmail.com>
2019-07-13 01:09:38 -0500

    Updates

---
+++ precache-manifest.ee7ae54a7dbe22570327b1a6402bf353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e135c95760359a38dfa640c7eea14c80",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "03cca9bdfbb234b8033b",
+    "url": "/ClickyGame/static/css/main.0e6c0a35.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "03cca9bdfbb234b8033b",
+    "url": "/ClickyGame/static/js/main.771410d6.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
